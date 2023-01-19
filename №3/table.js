b2339594594e8f360c8a57f9bf2c0f3a5ab07787
Timur <timurd2107@gmail.com>
2023-01-19 17:55:53 +0300

    Done!

---
+++ №3/table.js
@@ -0,0 +1,46 @@
+const table = document.createElement("table");
+let lineNumber=0;
+
+function addTable() {
+    if (document.getElementById('table') !=null){
+        alert("Уже есть");
+    }
+    else {
+
+        table.innerHTML = "<table>\n" +
+            "<tr>\n" +
+            "      <th>\n" +
+            lineNumber+
+            "      </td>\n" +
+            "     </td>\n" +
+            "      <td>\n" +
+            "      </td>\n" +
+            "</tr>"
+        "              </table>";
+
+        table.setAttribute('id','table')
+        document.body.append(table);
+        lineNumber++;
+    }
+}
+
+function addLine(){
+    let tab = table.insertRow();
+    tab.insertCell().append(lineNumber);
+    tab.insertCell().append("Хочу 5");
+    lineNumber++;
+}
+function deleteLine(){
+    if (document.getElementById('num').value=="") {
+        alert("Должно быть число, а не пустота");
+        return null;
+    }
+    num = document.getElementById('num').value;
+    try {
+        table.deleteRow(num);
+    }
+    catch (DOMexcepion){
+        alert("такой строки нет")
+    }
+
+}
